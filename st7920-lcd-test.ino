6adeef0aca5bc78c68bb1201a81e3bcb05b1e9a3
Will Holt <halbrd@gmail.com>
2017-09-04 13:09:25 +1000

    correct D1 Mini pin reference

--- st7920-lcd-test.ino
+++ st7920-lcd-test.ino
@@ -6,7 +6,7 @@
 // wiring notes
 // E=green, RW=purple, RS=yellow, RST=blue
 // Uno: E=13, RW=11, RS=10, RST=8
-// D1 Mini: E=1, RW=2, RS=3, RST=4
+// D1 Mini: E=5, RW=4, RS=0, RST=2
 
 // pin definitions - change these to match your wiring
 const int PIN_E = 13;
